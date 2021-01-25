5100f1242728af21b0a150d23ccd5305da6c817f
RameshRacharla <rameshracharlaa@gmail.com>
2021-01-25 09:59:02 +0530

    DB changes

---
+++ app/src/main/java/com/rameshracharla/notes/utils/DATABASE_VERSION.kt
@@ -0,0 +1,9 @@
+package com.rameshracharla.notes.utils
+
+val DATABASE_VERSION: Int = 1
+val DATABASE_NAME: String = "Jobs.db"
+val TABLE_NAME: String = "Jobs"
+
+val KEY_ID: String = "id"
+val KEY_JOB_NAME: String = "Job_name"
+val KEY_JOB_ASSIGNED_TIME: String = "Job_assigned_time"
